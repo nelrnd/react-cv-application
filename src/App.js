0f611dadab4eb5d93ecb52aff53fe153480bbefd
Nel <nel.renaudin@gmail.com>
2023-02-21 09:50:56 +0100

    Create EducationFormSection component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import GeneralFormSection from './components/GeneralFormSection';
+import EducationFormSection from './components/EducationFormSection';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <GeneralFormSection />
+        <EducationFormSection />
       </div>
     );
   }
---
+++ src/components/EducationFormSection.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class EducationFormSection extends Component {
+  render() {
+    return (
+      <div className="form-section">
+        <h2>Education</h2>
+
+        <div>
+          <label htmlFor="school-name">School name</label>
+          <input type="text" id="school-name" placeholder="The Odin Project" />
+        </div>
+
+        <div>
+          <label htmlFor="title-of-study">Title of study</label>
+          <input
+            type="text"
+            id="title-of-study"
+            placeholder="Full Stack JavaScript"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="date-of-study">Date of study</label>
+          <input type="text" id="date-of-study" placeholder="2020 - 2022" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EducationFormSection;
