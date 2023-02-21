dd0f40a70c9aafccb14371f8995cdf3ec5174064
Nel <nel.renaudin@gmail.com>
2023-02-21 10:40:21 +0100

    Create app state properties

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import EducationFormSection from './components/EducationFormSection';
 import WorkFormSection from './components/WorkFormSection';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      general: {
+        fullName: '',
+        jobTitle: '',
+        email: '',
+        phone: '',
+        address: '',
+      },
+      education: {
+        schoolName: '',
+        titleOfStudy: '',
+        dateOfStudy: '',
+      },
+      work: {
+        companyName: '',
+        positionTitle: '',
+        dateOfWork: '',
+        mainTasks: '',
+      },
+    };
+  }
   render() {
     return (
       <div className="App">
