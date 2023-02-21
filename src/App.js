8e4c67f9b955521020105192dfcbc2bb70fc4102
Nel <nel.renaudin@gmail.com>
2023-02-21 20:49:33 +0100

    Create MultiStepForm component and style MultiStepForm component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MultiStepForm from './components/MultiStepForm';
 import Form from './components/Form';
 import View from './components/View';
 
@@ -41,6 +42,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MultiStepForm />
+
         <Form data={this.state} handleChange={this.handleChange} />
 
         <View data={this.state} />
---
+++ src/components/MultiStepForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import GeneralFormSection from './GeneralFormSection';
+import EducationFormSection from './EducationFormSection';
+import WorkFormSection from './WorkFormSection';
+
+class MultiStepForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+    };
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>MultiStepForm</h1>
+
+        <div className="multi-step-form-control">
+          <button className="secondary">Go back</button>
+          <div className="progress-bar">
+            <div className="bar filled"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+          <button className="primary">Next step</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MultiStepForm;
