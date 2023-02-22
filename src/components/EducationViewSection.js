c41fbc9dee0723c123d9016b640574266acb1a3f
nel <nel.renaudin@gmail.com>
2023-02-22 15:51:59 +0100

    Refactor EducationViewSection and WorkViewSection components

--- src/components/EducationViewSection.js
+++ src/components/EducationViewSection.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class EducationViewSection extends Component {
   render() {
-    const { schoolName, titleOfStudy, dateOfStudy } = this.props.data;
     return (
-      <div className="view-section">
+      <div className='view-section'>
         <h3>Education</h3>
 
-        <p className="small">{dateOfStudy}</p>
+        {this.props.data.map((item, index) => (
+          <EducationViewUnit data={item} key={index} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class EducationViewUnit extends Component {
+  render() {
+    const { schoolName, titleOfStudy, dateOfStudy } = this.props.data;
+    return (
+      <div>
+        <p className='small'>{dateOfStudy}</p>
         <p>
           <strong>{schoolName}</strong>
         </p>
--- src/components/WorkViewSection.js
+++ src/components/WorkViewSection.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 
 class WorkViewSection extends Component {
   render() {
+    return (
+      <div className='view-section'>
+        <h3>Work experience</h3>
+
+        {this.props.data.map((item, index) => (
+          <WorkViewUnit data={item} key={index} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class WorkViewUnit extends Component {
+  render() {
     const { companyName, positionTitle, dateOfWork, mainTasks } =
       this.props.data;
     return (
-      <div className="view-section">
-        <h3>Work experience</h3>
-
-        <p className="small">{dateOfWork}</p>
+      <div>
+        <p className='small'>{dateOfWork}</p>
         <p>
           {positionTitle} at <strong>{companyName}</strong>
         </p>
