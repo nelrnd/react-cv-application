331036fd8b3f4f8550910e73012bb31122229258
Nel <nel.renaudin@gmail.com>
2023-02-28 16:31:55 +0100

    Create header, footer and main components

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Created by{' '}
+        <a href="https://github.com/nelrnd" target="_blank" rel="noreferrer">
+          Nel
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
