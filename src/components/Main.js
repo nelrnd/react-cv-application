37de62022fc820956e057f88c7164f6675114407
Nel <nel.renaudin@gmail.com>
2023-03-01 10:42:48 +0100

    Create Preview structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Form from './CVForm/Form';
+import Form from './Form/Form';
+import Preview from './Preview/Preview';
 
 function Main() {
   return (
     <main>
       <h1>Main</h1>
       <Form />
+      <Preview />
     </main>
   );
 }
---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import Main from './Main';
+
+function Preview() {
+  return (
+    <div>
+      <h1>Preview</h1>
+      <Sidebar />
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Preview;
