57299b8fd88fe6b247459b23efb2c7a13e1615fd
Nel <nel.renaudin@gmail.com>
2023-03-02 09:55:51 +0100

    Add GitHub icon in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GitHubIcon from '../assets/github-icon.svg';
 
 function Footer() {
   return (
     <footer>
       <p>
+        <img src={GitHubIcon} alt="GitHub icon" />
         Created by{' '}
         <a href="https://github.com/nelrnd" target="_blank" rel="noreferrer">
           Nel
